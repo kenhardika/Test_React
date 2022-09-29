ac8442480197152ecdc9e0fb172260101a977069
kenhardika <kenzamahardika@gmail.com>
2022-09-29 09:18:58 +0700

    update text

--- tour-tickets-web/src/App.js
+++ tour-tickets-web/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployment test react
+          Deployment test react, I deploy it twice now check it out
         </p>
       </header>
     </div>
