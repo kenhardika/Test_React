e97b82666160e32505a8cdbb1e009222b8ad6779
kenhardika <kenzamahardika@gmail.com>
2022-10-05 18:42:04 +0700

    add props and state test

--- tour-tickets-web/src/App.js
+++ tour-tickets-web/src/App.js
@@ -1,14 +1,39 @@
 import './App.css';
 import { Header } from './Header';
 import { Main } from './Main';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Main text = "this is from props"/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onClickBtn = this.onClickBtn.bind(this);
+    this.counterUp = this.counterUp.bind(this);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  onClickBtn(){
+    console.log('button clicked');
+  }
+  counterUp(){
+    this.setState({
+      count: this.state.count + 1,
+    });
+    console.log(this.state);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header/>
+        {/* instead of passing a string, we’re just passing a function below on onButtonClicked */}
+        <Main text = "this is from props" onButtonClicked = {this.onClickBtn}/> 
+         <button onClick={this.counterUp}> Counter Up </button>
+         <p>{this.state.count}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- tour-tickets-web/src/Main.js
+++ tour-tickets-web/src/Main.js
@@ -5,9 +5,18 @@ export class Main extends Component{
         super(props);
       }
     render(){
+        // console.log(this.props);
+        // with class component, use this inside render
+        // dissect property entering from outside to this.props{a,b} and assign a to text then b to onButtonClicked
+        const {text, onButtonClicked} = this.props; 
+        // console.log(text);
+        // console.log(onButtonClicked);
         return (
             <main className="App-main">
-                <h2> {this.props.text}</h2>
+                <h2> {text}</h2>
+                <div className="buttonLayer">
+                    <button onClick={onButtonClicked}> Click this nut </button>
+                </div>
             </main>
         ) 
     }
